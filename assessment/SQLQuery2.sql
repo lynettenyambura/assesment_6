5d489e001cf492321d1d06b44719b9f972386ecc
lynettenyambura <linetnyambura45@gmail.com>
2023-06-09 12:33:14 +0300

    assessment

--- assessment/SQLQuery2.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM Sales.customers;
-SELECT orders.order_id, customers.customername, staff.staffname
-FROM sales.customers
-JOIN customers ON orders.customer_id = customers.customer_id
-JOIN staff ON orders.staff_id = staff.staff_id;
